f695cc9d0a0fc34d8ecdab1eab506053ef29fd20
Deniz Senyurt <den.senyurt@gmail.com>
2016-09-21 23:24:03 -0400

    establish workflow abstraction

--- src/becPy/MicroDriver/RabbitMQDriver/RabbitMQDriver.py
+++ src/becPy/MicroDriver/RabbitMQDriver/RabbitMQDriver.py
@@ -5,32 +5,41 @@ class RabbitMQDriver():
 	
 	def __init__(self):
 		self.exchange = 'dsenyurt_test'
-		self.url = os.environ['AMQP_URL']
+		# self.url = os.environ['AMQP_URL']
+
+		self.url = "amqp://micro:password@localhost:5672/%2F"
 		parameters = pika.URLParameters(self.url)
+
+		# credentials = pika.PlainCredentials('micro', 'password')
+		# parameters = pika.ConnectionParameters(credentials=credentials, host='localhost')
+
 		self.connection = pika.BlockingConnection(parameters)
 		self.rabbit_channel = self.connection.channel()
-		self.rabbit_channel.exchange_declare(exchange = self.exchange, type='topic', durable=True)
-		self.rabbit_channel.queue_declare(queue='testing',durable=True)
+		self.rabbit_channel.exchange_declare(exchange=self.exchange, type='topic', durable=True)
+		self.rabbit_channel.queue_declare(queue='testing', durable=True)
 
-	def create_connection(self,options):
-		credentials = pika.PlainCredentials('micro','bench629')
-		parameters = pika.ConnectionParameters(credentials=credentials, host='green.benchmarkeducation.ny')
-		rabbit_connection = pika.TwistedProtocolConnection(parameters)
-		self.rabbit_channel = rabbit_connection.channel()
-		self.exchange = 'dsenyurt_test' 
+	# def create_connection(self,options):
+	# 	credentials = pika.PlainCredentials('micro','bench629')
+	# 	parameters = pika.ConnectionParameters(credentials=credentials, host='green.benchmarkeducation.ny')
+	# 	rabbit_connection = pika.TwistedProtocolConnection(parameters)
+	# 	self.rabbit_channel = rabbit_connection.channel()
+	# 	self.exchange = 'dsenyurt_test' 
 		
 
-	def send_message(self,options):
-		self.rabbit_channel.basic_publish(self.exchange,'testing',options)
+	def send_message(self, options):
+		self.rabbit_channel.basic_publish(exchange=self.exchange, 
+			routing_key='worker1', 
+			body=options, 
+			properties=pika.BasicProperties(delivery_mode=2))
 		# pass
 
 
-if __name__	== "__main__":
+if __name__ == "__main__":
 	rabbit = RabbitMQDriver()
 	cnt = 0
 	
 	while True:
-		rabbit.send_message("testing this queue" +str(cnt))
-		cnt+=1
+		rabbit.send_message("testing this queue" + str(cnt))
+		cnt += 1
 		print(cnt)
 		time.sleep(7)
--- src/becPy/Workflows/Reciever.py
+++ src/becPy/Workflows/Reciever.py
@@ -1,28 +1,41 @@
 import pika
 import os
 import time
+from WorkFlow import WorkFlow
 
-class Reciever():
+class Reciever(WorkFlow):
 
 	def __init__(self):
-		self.url = os.environ['AMQP_URL']
 		self.exchange = 'dsenyurt_test'
-		self.url = os.environ['AMQP_URL']
+		# self.url = os.environ['AMQP_URL']
+		# self.url = 'test'
+		self.url = "amqp://micro:password@localhost:5672/%2F"
 		parameters = pika.URLParameters(self.url)
+
+
+		# credentials = pika.PlainCredentials('micro', 'password')
+		# parameters = pika.ConnectionParameters(credentials=credentials, host='localhost')
+
 		self.connection = pika.BlockingConnection(parameters)
 		self.rabbit_channel = self.connection.channel()
-		self.rabbit_channel.queue_declare(queue='testing',durable=True)
+		self.rabbit_channel.queue_declare(queue='testing', durable=True)
+		self.rabbit_channel.queue_bind(exchange=self.exchange, queue='testing', routing_key='worker1')
+
+
+
 		
 
 	def create_connection(self):
 		pass
 
 	def recieve_message(self, ch, method, properties, body):
-		# print("Recieved a message:" + str(body))
-		print(self.url)
-		return ("Recieved a message: " + str(body))
+		print("Recieved a message:" + str(body))
+		# print(self.url)
+		# return ("Recieved a message: " + str(body))
 
 	def start_consuming(self):
+		# self.rabbit_channel.basic_consume(self.recieve_message, queue='testing', no_ack=True)
+		self.rabbit_channel.basic_qos(prefetch_count=1)
 		self.rabbit_channel.basic_consume(self.recieve_message, queue='testing', no_ack=True)
 		self.rabbit_channel.start_consuming()
 		# self.rabbit_channel.basic_consume(self.recieve_message,queue='testing',no_ack=True)
@@ -30,12 +43,14 @@ class Reciever():
 
 if __name__ == "__main__":
 	rc = Reciever()
-	rc.create_connection()
+	# rc.create_connection()
+	print("Before Consuming")
+	rc.start_consuming()
+	print("After Consuming")
 
 	while True:
-		result = rc.start_consuming()
-		print("result test" + str(result))
+		print("result test")
 		time.sleep(5)
-		pass
+		
 	
 	
